7467a64c125d56a07c0677846133283030b5e52d
Định Lê Trí <letridinh2002@gmail.com>
2022-08-14 12:03:27 +0700

    complete exp component css, responsive design

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,94 @@
 import React from 'react'
 import './experience.css'
+import { AiFillHtml5 } from 'react-icons/ai'
+import { DiCss3, DiJavascript1 } from 'react-icons/di'
+import { FaBootstrap, FaNodeJs, FaPython } from 'react-icons/fa'
+import { BsFillPatchCheckFill } from 'react-icons/bs'
+import { SiTailwindcss, SiMongodb, SiMysql } from 'react-icons/si'
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className='container experience__container'>
+        <div className="experience__frontend">
+          <h3>Front-End Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <AiFillHtml5 className='experience__details-icon'/>
+              <div>
+                <h4>HTML <BsFillPatchCheckFill /></h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <DiCss3 className='experience__details-icon'/>
+              <div>
+                <h4>CSS <BsFillPatchCheckFill /></h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <FaBootstrap className='experience__details-icon'/>
+              <div>
+                <h4>Bootstrap <BsFillPatchCheckFill /></h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <DiJavascript1 className='experience__details-icon'/>
+              <div>
+                <h4>JavaScript <BsFillPatchCheckFill /></h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <SiTailwindcss className='experience__details-icon'/>
+              <div>
+                <h4>Tailwind <BsFillPatchCheckFill /></h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* END OF FRONTEND */}
+        <div className="experience__backend">
+          <h3>Back-End Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <FaNodeJs className='experience__details-icon'/>
+              <div>
+                <h4>NodeJS <BsFillPatchCheckFill /></h4>
+                <small className='text-light'>Senior</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <SiMongodb className='experience__details-icon'/>
+              <div>
+                <h4>MongoDB <BsFillPatchCheckFill /></h4>
+                <small className='text-light'>Experienced</small>s
+              </div>
+            </article>
+            <article className='experience__details'>
+              <SiMysql className='experience__details-icon'/>
+              <div>
+                <h4>MySQL <BsFillPatchCheckFill /></h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <FaPython className='experience__details-icon'/>
+              <div>
+                <h4>Python <BsFillPatchCheckFill /></h4>
+                <small className='text-light'>Senior</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import { AiTwotoneHome, AiOutlineUserSwitch } from 'react-icons/ai'
+import { MdWork, MdMiscellaneousServices } from 'react-icons/md'
+import { TiContacts } from 'react-icons/ti'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('# ')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiTwotoneHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUserSwitch /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><MdWork /></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdMiscellaneousServices /></a>
+      <a href="#contact" onClick={() => {setActiveNav('#contact')}} className={activeNav === '#contact' ? 'active' : ''}><TiContacts /></a>
+    </nav>
   )
 }
 
