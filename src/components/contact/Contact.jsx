abec03d1a13e39108d39b7cd7e5e5b9851dec30f
Định Lê Trí <letridinh2002@gmail.com>
2022-08-17 12:54:20 +0700

    contact comp, code form for submit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import './contact.css'
+import { MdMarkEmailUnread } from 'react-icons/md'
+import { BsMessenger, BsInstagram } from 'react-icons/bs'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdMarkEmailUnread className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>findxcode1125@gmail.com</h5>
+            <a href="mailto:findxcode1125@gmail.com" target="_blank">Send Mail</a>
+          </article>
+          <article className="contact__option">
+            <BsMessenger className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Định Lê</h5>
+            <a href="https://m.me/100059112795031" target="_blank">Chat Now</a>
+          </article>
+          <article className="contact__option">
+            <BsInstagram className='contact__option-icon'/>
+            <h4>Instagram</h4>
+            <h5>_Jjindin</h5>
+            <a href="https://www.instagram.com/direct/t/_jjindin/" target="_blank">Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
