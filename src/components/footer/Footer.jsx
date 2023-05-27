bbce3f01ce66c01ca5f8ac471cd9164d6b64022b
Định Lê Trí <letridinh2002@gmail.com>
2023-05-27 11:34:40 +0700

    up

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,13 +5,13 @@ import { BsFacebook, BsInstagram, BsLinkedin } from 'react-icons/bs'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>_JJINDIN - LEE </a>
+      <a href="#" className='footer__logo'>FLAX22 - LE DINH </a>
 
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
-        <li><a href="#services">Services</a></li>
+        {/* <li><a href="#services">Services</a></li> */}
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#testimonials">Testimonials</a></li>
         <li><a href="#contact">Contact</a></li>
